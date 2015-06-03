f1163827c4173574e01455feba525906b597c0cd
wjrobinson11 <wes@socialcompass.net>
2015-06-03 16:48:35 +0200

    Add ingredient button working

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -3,7 +3,10 @@ $(document).ready( function(){
     console.log($('.rli-form-recipe-line-item-container').html())
     var html = $('.rli-form-recipe-line-item:first').clone()
     var newSelectIngredientName = "recipe[recipe_line_items_attributes][" + $('.rli-form-recipe-line-item').length + "][ingredient_id]"
-    $(html).children('.field').children('select').attr('name', newSelectIngredientName).val('null')
+    var newQuantityName = "recipe[recipe_line_items_attributes][" + $('.rli-form-recipe-line-item').length + "][quantity]"
+    $(html).children('.field').children('select.ingredients-dropdown').attr('name', newSelectIngredientName).val('')
+    $(html).children('.rli-form-quantity-container').children('.field').children('.rli-quantity').attr('name', newQuantityName).attr('value','')
+    $(html).children('.rli-form-quantity-container').children('.rli-form-unit-of-measurement-container').html('')
     $(html).appendTo('.rli-form-recipe-line-items-container')
   });
 });
