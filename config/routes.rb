991767811d6082c20e50dc53b15105148f4b705e
wjrobinson11 <wes@socialcompass.net>
2015-06-09 11:34:07 +0200

    root to menus index

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :ingredients
 
-  root 'pages#menu'
+  root 'menus#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
